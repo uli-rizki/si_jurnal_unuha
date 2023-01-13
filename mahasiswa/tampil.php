e520c54e2b3d0dd9d69907c9d7e9b3cb645af345
uli <ulirizki@gmail.com>
2023-01-13 15:28:14 +0700

    tambah tampil mahasiswa

--- mahasiswa/tampil.php
+++ mahasiswa/tampil.php
@@ -4,6 +4,11 @@ include_once('../database/koneksi.php');
 
 require "../layout/header.php";
 require "../layout/navbar.php";
+
+$query_mhs = "SELECT * FROM mahasiswa";
+$mahasiswa = $koneksi->query($query_mhs);
+$koneksi->close();
+
 ?>
 
 <div class="wrap">
@@ -21,6 +26,24 @@ require "../layout/navbar.php";
                         <th>Aksi</th>
                     </tr>
                 </thead>
+                <tbody>
+                <?php 
+                if ($mahasiswa->num_rows > 0) {
+                    while($row = $mahasiswa->fetch_assoc()) {
+                ?>
+                <tr>
+                    <td><?php echo $row['nim']; ?></td>
+                    <td><?php echo $row['nama_lengkap']; ?></td>
+                    <td><?php echo $row['asal_kelas']; ?></td>
+                    <td>
+                        <button>Edit</button> | <button>Hapus</button>
+                    </td>
+                </tr>
+                <?php 
+                    }
+                }
+                ?>
+                </tbody>
             </table>
         </div>
     </div>
